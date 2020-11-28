4a86ca1b380bd491ccee82cd9e92bba5d13b3f9e
Mujtaba kamaL rahseed <74490076+Mujtaba-rasheed@users.noreply.github.com>
2020-11-28 23:32:40 +0200

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,26 @@
+<! DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewpor" content=" width=device-width, initial-scale=1.8">
+    <title>   </title>
+    <link rel="stylesheet" type="text/css" href="style3.css">   
+</head>
+<body>
+        <center> 
+    <img src="f77.png" class="avatar">
+                
+    <button>
+      
+        <a href="index20.php"target="-blank"><p>الرجوع الا شاشة المدير</p></a> 
+        </button>
+    </center>
+
+        
+<?php
+?>
+
+       <br>
+        
+</body>
+</html>
